7567a1591f91f5039a18081e25254803b70711f4
Timothy Farrell <tim@thecookiejar.me>
2020-10-01 14:37:53 -0500

    Abstract flag data type conversion so we can reject things that don't...
    look like a boolean value.

--- app/cascade_types.py
+++ app/cascade_types.py
@@ -1,3 +1,4 @@
+from distutils.util import strtobool
 from datetime import datetime
 from typing import Union, Literal, Dict
 from uuid import UUID
@@ -6,3 +7,15 @@ from uuid import UUID
 FLAG_VALUE_TYPE_NAMES = Literal['bool', 'str', 'int', 'datetime']
 FLAG_VALUE_TYPE = Union[bool, str, int, datetime]
 FLAG_REVISIONED_VALUE_TYPE = Dict[Literal['value', 'revision'], Union[FLAG_VALUE_TYPE, UUID]]
+
+
+def convert(value: FLAG_VALUE_TYPE, type_name: FLAG_VALUE_TYPE_NAMES):
+    typ = eval(type_name)
+
+    if isinstance(value, typ):
+        return value
+
+    if type_name == 'bool':
+        return strtobool(value)
+
+    return typ(value)
--- app/models/flagdefinition.py
+++ app/models/flagdefinition.py
@@ -1,6 +1,6 @@
 from pydantic import validator, BaseModel
 
-from ..cascade_types import FLAG_VALUE_TYPE, FLAG_VALUE_TYPE_NAMES
+from ..cascade_types import FLAG_VALUE_TYPE, FLAG_VALUE_TYPE_NAMES, convert
 
 
 class FlagDefinition(BaseModel):
@@ -11,12 +11,13 @@ class FlagDefinition(BaseModel):
     default_value: FLAG_VALUE_TYPE
 
     @validator('default_value')
-    def default_value_as_datatype(cls, v, values):
-        type_name = values['datatype']
-        typ = eval(type_name)
-        if not isinstance(v, typ):
-            try:
-                return typ(v)
-            except (TypeError, ValueError):
-                raise ValueError(f'Cannot convert "{v}" into {type_name}.')
-        return v
+    def default_value_as_datatype(cls, value, values, config, field):
+        try:
+            type_name = values['datatype']
+        except KeyError:
+            raise ValueError('Datatype missing')
+
+        try:
+            return convert(value, type_name)
+        except (TypeError, ValueError):
+            raise ValueError(f'Cannot convert "{value}" into {type_name}.')
